55e92b5fe23945de2ad3799c75aad008ecde4993
Jose Estrella <jose.estrella@imploie.com>
2021-01-07 04:45:09 -0700

    dynamic inputs

--- src/app/dynamicinputs/dynamicinputs.component.ts
+++ src/app/dynamicinputs/dynamicinputs.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 export class InputResult {
   public inputName: string;
@@ -15,32 +15,23 @@ export class DynamicInputs implements OnInit {
   @Output() inputResults: EventEmitter<InputResult[]> = new EventEmitter<
     InputResult[]
   >();
-  field: FormArray;
+  inputForm: FormGroup;
 
-  inputForm = this.fb.group({
-    fields: this.fb.array([this.fb.control('')]),
-  });
-
-  get fields() {
-    return this.inputForm.get('fields') as FormArray;
+  constructor(private fb: FormBuilder) {
+    this.inputForm = fb.group({});
   }
-
-  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.inputFields.map((field) => {
-      this.fields.push(this.fb.control(field));
+      this.inputForm.addControl(field, new FormControl(''));
     });
   }
 
-  addAlias() {
-    this.fields.push(this.fb.control(''));
-  }
+  onSubmit() {
+    console.log(this.inputForm);
 
-  onSubmit(f) {
-    console.log(this.inputForm.value);
-    console.log(f);
-    console.log('Submitted');
+    
+    
     // this.inputResults.next([
     //   { inputName: 'Name', inputValue: 'value' },
     //   { inputName: 'City', inputValue: 'value' },
